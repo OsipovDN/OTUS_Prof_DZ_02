3fec382cc4f0d1dc94fa98883f1fadfc15ed8d83
OsipovDN <didison88@gmail.com>
2023-04-10 11:15:12 +0300

    Сортировка по байтам

--- header/LexSort.h
+++ header/LexSort.h
@@ -3,14 +3,15 @@
 #include <vector>
 #include <algorithm>
 #include <iostream>
+#include <execution>
 
 class LexSort {
 private:
 	using TabInt = std::vector < std::vector < int >>;
 	std::unique_ptr<TabInt> ip_tab_trans;
 public:
-	LexSort(TabInt obj):
-		ip_tab_trans(std::make_unique<TabInt>(obj)){};
+	LexSort(TabInt obj) :
+		ip_tab_trans(std::make_unique<TabInt>(obj)) {};
 
 	LexSort(LexSort&) = delete;
 	LexSort operator =(LexSort&) = delete;
@@ -20,45 +21,65 @@ public:
 	~LexSort() {};
 
 	void sortForw() {
-		std::stable_sort(ip_tab_trans->begin(), ip_tab_trans->end(),
-			[](std::vector<int> rvl, std::vector<int>lvl) {
-				return  rvl < lvl;
-			});
+		std::sort(ip_tab_trans->begin(), ip_tab_trans->end());
 	};
 
 	void sortRev() {
-		std::stable_sort(ip_tab_trans->begin(), ip_tab_trans->end(),
-			[](std::vector<int> rvl, std::vector<int>lvl) {
-				return  rvl > lvl;
-			});
+		std::sort(ip_tab_trans->rbegin(), ip_tab_trans->rend());
 	};
 
-	void sortOf() {
-	
+	TabInt sortOf(std::pair <int, int>& b1, std::pair <int, int>& b2) {
+		bool flag_sort;
+		TabInt res_vec;
+		flag_sort = std::is_sorted(std::execution::par, ip_tab_trans->cbegin(), ip_tab_trans->cend());
+		if (!flag_sort)
+			sortForw();
+		auto start_it = std::find_if(ip_tab_trans->cbegin(), ip_tab_trans->cend(), [&](std::vector <int> ip) {
+			return (ip[b1.first - 1] == b1.second && ip[b2.first - 1] == b2.second);
+			});
+		auto end = std::count_if(start_it, ip_tab_trans->cend(), [&](std::vector <int> ip) {
+			return (ip[b1.first - 1] == b1.second && ip[b2.first - 1] == b2.second);
+			});
+		std::for_each(start_it, start_it + end, [&](const std::vector<int>& obj) {
+			return res_vec.push_back(obj);
+			});
+		return std::move(res_vec);
+
 	};
+
+	TabInt sortOf(std::pair <int, int>& b) {
+		bool flag_sort;
+		TabInt res_vec;
+		flag_sort = std::is_sorted(std::execution::par, ip_tab_trans->cbegin(), ip_tab_trans->cend());
+		if (!flag_sort)
+			sortForw();
+		if (b.first == 0) {
+
+			std::for_each(ip_tab_trans->cbegin(), ip_tab_trans->cend(), [&](const std::vector<int>& ip) {
+				auto flag = std::find_if(ip.cbegin(), ip.cend(), [&](const int& val) {return val == b.second; });
+				if (flag!= ip.cend()) {
+					res_vec.push_back(ip);
+				}
+				});
+			return std::move(res_vec);
+		}
+		else {
+			auto start_it = std::find_if(ip_tab_trans->cbegin(), ip_tab_trans->cend(), [&](std::vector <int> ip) {
+				return (ip[b.first - 1] == b.second);
+				});
+			auto end = std::count_if(start_it, ip_tab_trans->cend(), [&](std::vector <int> ip) {
+				return (ip[b.first - 1] == b.second);
+				});
+			std::for_each(start_it, start_it + end, [&](const std::vector<int>& ip) {
+				return res_vec.push_back(ip);
+				});
+			return std::move(res_vec);
+		}
+
+	};
+
 	std::vector<std::vector<int>>& getTab() {
 		return *this->ip_tab_trans;
 	};
 
-	
-
-
-
-	/*std::vector<std::vector<std::string>> sort() {
-
-	std::stable_sort(vec.begin(), vec.end(),
-		[](std::vector<std::string> rvl, std::vector<std::string>lvl) {
-			if (std::stoi(rvl[0]) == std::stoi(lvl[0])) {
-				if (std::stoi(rvl[1]) == std::stoi(lvl[1])) {
-					if (std::stoi(rvl[2]) == std::stoi(lvl[2])) {
-						return std::stoi(rvl[3]) > std::stoi(lvl[3]);
-					}
-					return std::stoi(rvl[2]) > std::stoi(lvl[2]);
-				}
-				return std::stoi(rvl[1]) > std::stoi(lvl[1]);
-			}
-			return std::stoi(rvl[0]) > std::stoi(lvl[0]);
-		});
-	return vec;
-}*/
 };
